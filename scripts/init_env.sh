25462e9f398a7b0224511a5c57fe848f149d9dda
Joseph A. Lutz <CipherWizard@cipherwizard.net>
2017-02-23 01:54:31 -0600

    Allow for cherrypy ssl and remove MySQL module
    Signed-off-by: Joseph A. Lutz <CipherWizard@cipherwizard.net>

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -6,6 +6,10 @@ pip install -Iv \
   cherrypy==3.8.0 \
   Mako==1.0.4 \
   WTForms==2.1 \
-  MySQL-python \
   iptools==0.6.1 \
+  cython \
+  pyOpenSSL \
+
+openssl genrsa -out privkey.pem 2048
+openssl req -new -x509 -days 365 -key privkey.pem -out cert.pem
 
